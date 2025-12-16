6d1882c537a985925ad24f66e3d693c92122c824
Lucas Albuquerque <lucasalbuquerque0408@gmail.com>
2025-12-15 23:32:46 -0300

    adiciona front inicial para testes e adiciona npm install no setup.sh

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/app', function () {
+    return view('layouts.app');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+});
+
+Route::get('/home', function () {
+    return view('dashboard.home');
+});
